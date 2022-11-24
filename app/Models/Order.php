671fee2da4be29f34cfeb5ef32b404b763c44f58
Ruben Viana <up202005108@up.pt>
2022-11-24 10:41:43 +0000

    fds

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
