ae4039543216df0242482d453f1b2ff90b7018cf
pedronunomacedo <pedronunomacedo@hotmail.com>
2022-12-07 20:25:01 +0000

    Changed login page

--- storage/framework/views/06d2d66f84432056d533362d942460427a0312e4.php
+++ storage/framework/views/06d2d66f84432056d533362d942460427a0312e4.php
@@ -1,35 +1,43 @@
 <?php $__env->startSection('content'); ?>
-<form class="form-group bg-dark bg-opacity-25 p-3 mx-auto mt-5" method="POST" action="<?php echo e(route('login')); ?>" style="width: 20em">
-    <?php echo e(csrf_field()); ?>
+    <div id="login_page_content">
+        <div id="main_image_div">
+            <img src="<?php echo e(url('/images/login_page.png')); ?>" id="main_image"/>
+        </div>
+        <div id="login_register_content">
+            <h1>Log In</h1>
+            <form class="form-group bg-light bg-opacity-25 mx-auto mt-5" method="POST" action="<?php echo e(route('login')); ?>">
+                <?php echo e(csrf_field()); ?>
 
-    <div class="form-floating mb-3">
-        <input type="email" class="form-control" id="email" name="email" value="<?php echo e(old('email')); ?>" required placeholder="email">
-        <label for="email">Email</label>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" id="email" name="email" value="<?php echo e(old('email')); ?>" required placeholder="email">
+                    <label for="email">Email</label>
+                </div>
+                <?php if($errors->has('email')): ?>
+                    <span class="error">
+                    <?php echo e($errors->first('email')); ?>
+
+                    </span>
+                <?php endif; ?>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" id="password" name="password" required placeholder="password">
+                    <label for="password">Password</label>
+                </div>
+                <?php if($errors->has('password')): ?>
+                    <span class="error">
+                        <?php echo e($errors->first('password')); ?>
+
+                    </span>
+                <?php endif; ?>
+                <label>
+                    <input type="checkbox" class="form-check-input" name="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>> Remember Me</input>
+                </label>
+                <div class="buttons" id="butons_login_page">
+                    <button class="btn btn-dark" type="submit">Login</button>
+                    <button class="btn btn-outline-dark" id="register_button" href="<?php echo e(route('register')); ?>">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
-    <?php if($errors->has('email')): ?>
-        <span class="error">
-        <?php echo e($errors->first('email')); ?>
-
-        </span>
-    <?php endif; ?>
-    <div class="form-floating mb-3">
-        <input type="password" class="form-control" id="password" name="password" required placeholder="password">
-        <label for="password">Password</label>
-    </div>
-    <?php if($errors->has('password')): ?>
-        <span class="error">
-            <?php echo e($errors->first('password')); ?>
-
-        </span>
-    <?php endif; ?>
-    <label>
-        <input type="checkbox" class="form-check-input" name="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>> Remember Me
-    </label>
-    <button class="btn btn-dark" type="submit">
-        Login
-    </button>
-    <a class="btn btn-outline-dark" href="<?php echo e(route('register')); ?>">Register</a>
-</form>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/pedromacedo/Desktop/lbaw2284/resources/views/auth/login.blade.php ENDPATH**/ ?>
--- storage/framework/views/f79b680e14a0cbb6bbe08fd44fda37a895b06f80.php
+++ storage/framework/views/f79b680e14a0cbb6bbe08fd44fda37a895b06f80.php
@@ -11,7 +11,11 @@
     <title><?php echo e(config('app.name', 'Laravel')); ?></title>
 
     <!-- CSS only -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@800&display=swap" rel="stylesheet"> 
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/loginAndregister.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
