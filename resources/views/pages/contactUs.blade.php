77eac43c28e50a81ebe6a93a32aef0d413d55eed
pedronunomacedo <pedronunomacedo@hotmail.com>
2023-01-03 23:48:49 +0000

    Final commit

--- resources/views/pages/contactUs.blade.php
+++ resources/views/pages/contactUs.blade.php
@@ -12,7 +12,7 @@
                 <p class="mb-5">Here's how to reach us!</p>
                 <strong><p><i class="fa fa-phone"></i> 912345678</p></strong>
                 <strong><p><i class="fa fa-envelope"></i> admin@tech4you.com</p></strong>
-                <strong><p><i class="fa fa-building"></i> Faculdade de Engharia da Universidade do Porto</p></strong>
+                <strong><p><i class="fa fa-building"></i> Faculdade de Engenharia da Universidade do Porto</p></strong>
             </div>
             <div class="col-md-6">
                 <img src="{{ url('/images/contact_us_page.png') }}" id="about_us_image" style="max-width: 30rem" />
